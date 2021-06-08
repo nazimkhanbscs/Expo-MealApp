deea6a6283828ae9f541fadbaaeb3550bce6830f
nazimkhanbscs <nazimkhanbscsmscsphd@gmail.com>
2021-06-08 11:18:10 +0500

    Tuesday first commit on branch

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { HeaderButton } from 'react-navigation-header-buttons';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import { Platform } from "react-native";
+import { HeaderButton } from "react-navigation-header-buttons";
+import { Ionicons } from "@expo/vector-icons";
 
-import Colors from '../constants/Colors';
-
-const CustomHeaderButton = props => {
+import Colors from "../constants/Colors";
+// this component is currently useless
+const CustomHeaderButton = (props) => {
   return (
     <HeaderButton
       {...props}
       IconComponent={Ionicons}
       iconSize={23}
-      color={Platform.OS === 'android' ? 'white' : Colors.primaryColor}
+      color={Platform.OS === "android" ? "white" : Colors.primaryColor}
     />
   );
 };
